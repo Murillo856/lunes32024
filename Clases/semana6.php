fb8d0e0ab68d851558544ec476cf42419eb31575
klealufide <kleal40321@ufide.ac.cr>
2024-10-22 00:54:05 -0600

    Variables

--- Clases/semana6.php
+++ Clases/semana6.php
@@ -7,6 +7,17 @@
 </head>
 
 <body>
+<h1>Hola semana 6</h1>
+<?php 
+    $edad = 50;
+    $edad2 = 60;
+    $total = $edad + $edad2;
+    echo $total."<br>";
 
+    $nombre = "Karol ";
+    $edad = 34;
+
+    echo "Mi nombre es ".$nombre." , mi edad es de ".$edad;
+?>
 </body>
 </html>
