46b1b58223572e104d6acbd6a3c1e5c5822e0ced
klealufide <kleal40321@ufide.ac.cr>
2024-11-25 18:48:45 -0600

    Semana 11

--- Clases/script10.js
+++ Clases/script10.js
@@ -16,4 +16,40 @@ $(function () {
         $("#saludo").css("color","orange");
         $("#imagen").attr("src","images.jpg");
     });
+
+    console.log("Semana 11");
+
+
+    $("#aparecer").on("click", function () {
+        $("#miCuadrado").fadeIn();
+    });
+    $("#desaparecer").on("click", function () {
+        $("#miCuadrado").fadeOut();
+    });
+
+    $("#alternar").on("click", function () {
+        $("#miCuadrado").toggleClass("red");
+    });
+
+    $("#animar").on("click", function () {
+        $("#miCuadrado").animate({
+            width: "500px",
+            height: "500px",
+            opacity: 0.8
+        }, 1000);
+    });
+
+
+    $("#inicio").on("click", function () {
+        $("#contenido").prepend("<h3>Agregar al inicio</h3>");
+    });
+    $("#final").on("click", function () {
+        $("#parrafo").append("<br>Agregar al final");
+    });
+    $("#tareaFinal").on("click", function () {
+        $("#listaTarea").append("<li>"+$("#tarea").val()+"</li>");
+        $("#tarea").val("");
+    });
+
+    
 });
