bde61a856fc93be42f15d1da78e4474b1a101eae
klealufide <kleal40321@ufide.ac.cr>
2024-11-25 19:56:46 -0600

    Semana 11 Contacto

--- matriculalinea/contact.php
+++ matriculalinea/contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<?php 
+<?php
 include("head.php")
 ?>
 
@@ -10,7 +10,33 @@ include("head.php")
         <?php include("menu.php") ?>
     </header>
     <main>
+        <section>
+            <form action="procesar_contacto.php" method="post" id="contactForm">
+                <h2>Contacto</h2>
+                <div class="form-group">
+                    <label for="contact">Nombre de Contacto</label>
+                    <input type="text" id="contact" name="contact">
+                    <span id="error-contact">El nombre es obligatorio.</span>
+                </div>
+                <div class="form-group">
+                    <label for="email">Correo Electrónico</label>
+                    <input type="email" id="email" name="email">
+                    <span id="error-email">El correo es obligatorio.</span>
+                </div>
+                <div class="form-group">
+                    <label for="subject">Asunto</label>
+                    <input type="text" id="subject" name="subject">
+                    <span id="error-subject">El asunto es obligatorio.</span>
+                </div>
+                <div class="form-group">
+                    <label for="message">Mensaje</label>
+                    <textarea id="message" name="message" rows="5"></textarea>
+                    <span id="error-message">El mensaje es obligatorio.</span>
 
+                </div>
+                <button type="submit" class="boton">Enviar</button>
+            </form>
+        </section>
     </main>
 
     <?php include("footer.php") ?>
